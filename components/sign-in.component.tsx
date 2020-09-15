d238e60497da862b1ba95e168cef18ba1d0f8fca
ipaullly <ipaully22@gmail.com>
2020-09-15 21:54:39 +0300

    [finishes #174529839] set up mobile app github directory

---
+++ components/sign-in.component.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native'
+
+const WelcomePage = () => {
+	const handlePress = () => {
+		// navigate to sign up form
+	}
+
+	const handleLoginRedirect = () => {
+		// navigate to login options page
+	}
+
+  return (
+		<View style={styles.container}>
+			<Text
+				style={styles.header}
+			>Welcome</Text>
+			<Text
+				style={styles.instruction}
+			>Please login or Sign Up to continue using our App</Text>
+			<View style={styles.imgBack}>
+				<View
+					style={styles.img}
+				/>
+			</View>
+			<Button
+				title='Create Account'
+				onPress={() => handlePress()}
+			/>
+			<View style={styles.loginText}>
+				<Text>Already have an account?</Text>
+				<TouchableOpacity
+					onPress={() => handleLoginRedirect()}
+				>
+					<Text
+						style={styles.loginLink}
+					>Login</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
+
+export default WelcomePage;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		marginTop: 30,
+		paddingLeft: 25,
+		paddingRight: 25
+	},
+	header: {
+		fontSize: 30,
+		paddingBottom: 10,
+	},
+	instruction: {
+		fontSize: 12,
+		marginTop: -80,
+		width: '80%',
+		letterSpacing: 2
+	},
+	img: {
+		width: '70%',
+		textAlign: 'center',
+		height: '30vh',
+		backgroundColor: 'pink'
+	},
+	imgBack: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginTop: 10,
+		marginBottom: 10
+	},
+	loginText: {
+		display: 'flex',
+		flexDirection: 'row',
+		marginTop: -50,
+		letterSpacing: 2
+	},
+	loginLink: {
+		textDecorationLine: 'underline',
+		paddingLeft: 10
+	}
+})
